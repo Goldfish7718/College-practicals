5d03df9f2d0287afc0658fc16f287e5032684e19
Tejas Nanoti <tejasnanoti2@gmail.com>
2023-09-16 23:42:09 +0530

    Complete triplet function in Assignment 4

--- DSA/assignment4.cpp
+++ DSA/assignment4.cpp
@@ -3,11 +3,11 @@ using namespace std;
 
 class SparseMatrix {
     public:
-        int r, c, i, j, idx = 0, non_zero_quantity = 0;
+        int r, c, i, j, idx = 1, non_zero_quantity = 0;
         int sparse[20][20];
         int triplet[20][20];
         int non_zero_elems[20];
-        int row_col_matrix[20][2];
+        int row_col_matrix[20][3];
 
         public:
             void getData() {
@@ -24,14 +24,18 @@ class SparseMatrix {
             }
 
             void process_triplet() {
+
+                row_col_matrix[0][0] = r;
+                row_col_matrix[0][1] = c;
+
                 for (i = 0; i < r; i++) {
                     for (j = 0; j < c; j++) {
                         if (sparse[i][j] != 0)  {
                             non_zero_quantity++;
 
-                            non_zero_elems[idx] = sparse[i][j];
-                            row_col_matrix[idx][0] = i;
-                            row_col_matrix[idx][1] = j;
+                            row_col_matrix[idx][0] = i + 1;
+                            row_col_matrix[idx][1] = j + 1;
+                            row_col_matrix[idx][2] = sparse[i][j];
 
                             idx++;
                         }
@@ -39,38 +43,20 @@ class SparseMatrix {
                     }
                 }
 
-            //    for (i = 0; i < non_zero_quantity; i++) {
-            //         for (j = 0; j < 2; j++) {
-            //             triplet[i][j] = 
-            //         }
-            //    }
+                cout << "\n\n";
+
+                row_col_matrix[0][2] = non_zero_quantity;
+
+                for (i = 0; i <= non_zero_quantity; i++) {
+                    for (j = 0; j < 3; j++) {
+                        cout << row_col_matrix[i][j] << " ";
+                    }
+                    cout << endl;
+                }
             }
 };
 
 int main () {
-    // int mat1[20][20], i, j, n, mat2[20][20], add[20][20];
-    
-    
-
-    // cout << "Enter elements of second matrix" << endl;
-
-    // for (i = 0; i < n; i++) {
-    //     for (j = 0; j < n; j++) {
-    //         cin >> mat2[i][j];
-    //     }
-    // }
-
-    // cout << "The addition of the two matrix" << endl;
-
-    // for (i = 0; i < n; i++) {
-    //     for (j = 0; j < n; j++) {
-    //         add[i][j] = mat1[i][j] + mat2[i][j];
-    //         cout << add[i][j] << " ";
-    //     }
-    //     cout << endl;
-    // }
-
-    // return 0;
 
     SparseMatrix s;
     s.getData();
